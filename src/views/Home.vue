8c6eef40f403a129443b5941a67c21b87a09d2d7
Pau Abella <pauabellamolina1@gmail.com>
2021-01-02 22:59:55 +0100

    Changed Spotify's login redirect url to the production url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,9 +14,9 @@
     methods: {
       loginSpotify() {
         /* DEV */
-        window.location.href = "https://accounts.spotify.com/authorize?client_id=a432d4cd207345bf9bbb9be8b773571b&redirect_uri=http://localhost:8080&scope=user-top-read&response_type=token";
+        // window.location.href = "https://accounts.spotify.com/authorize?client_id=a432d4cd207345bf9bbb9be8b773571b&redirect_uri=http://localhost:8080&scope=user-top-read&response_type=token";
         /* PROD */
-        // window.location.href = "https://accounts.spotify.com/authorize?client_id=a432d4cd207345bf9bbb9be8b773571b&redirect_uri=https://instafy.me&scope=user-top-read&response_type=token";
+        window.location.href = "https://accounts.spotify.com/authorize?client_id=a432d4cd207345bf9bbb9be8b773571b&redirect_uri=https://findyourclique.pauabella.dev/&scope=user-top-read&response_type=token";
       }
 
     }
